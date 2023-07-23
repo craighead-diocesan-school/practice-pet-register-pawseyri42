b637c57ccd03459617461dd4e02c32c5cd32a5e9
Riley <124632055+pawseyri42@users.noreply.github.com>
2023-07-23 22:00:13 +0000

    end of lesson

---
+++ script.js
@@ -0,0 +1,9 @@
+function animals () {
+
+    let name = prompt(" What is the name of the animal you are wanting to record? ")
+
+    let speices = prompt ("")
+
+    let age = prompt (" What is the age of " + name )
+
+}
