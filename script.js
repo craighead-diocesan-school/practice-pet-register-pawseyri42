39337b08051808eb75c87b9239cb1c2c9fdbfe36
Riley <124632055+pawseyri42@users.noreply.github.com>
2023-07-24 22:05:05 +0000

    Questions done

--- script.js
+++ script.js
@@ -2,8 +2,8 @@ function animals () {
 
     let name = prompt(" What is the name of the animal you are wanting to record? ")
 
-    let speices = prompt ("")
+    let speices = prompt (" what type of animal is " + name )
 
-    let age = prompt (" What is the age of " + name )
+    let age = prompt (" What is the age of your " + speices )
 
 }
