a00b34ead784d85b9895853c86935f2c5354d13f
Riley <124632055+pawseyri42@users.noreply.github.com>
2023-07-30 21:23:50 +0000

    cleaned up

--- script.js
+++ script.js
@@ -1,20 +1,26 @@
-let nameArray = [ 
-'tiger',
-'Mouse',
-'Edward',
-'Cotter',
+
+let nameArray = [
+  { name: 'Tiger', age: 1, species: 'dog' },
+  { name: 'Mouse', age: 5, species: 'horse' },
+  { name: 'bubba', age: 3, species: 'lamb' },
+  { name: 'Cotter', age: 19, species: 'horse' },
+
 ]
 
-function addName() {
-    // lets user input the name of the pet
-    let name = prompt("What is the name of the pet?")
-    // add a new name to add to array of names
-    nameArray.push(name)
+function addPet() {
+  // lets user input the name of the pet
+  let animalName = prompt("What is the name of the pet?")
+  // add a new name to add to array of names
+  nameArray.push(animalName)
 }
 
-function names() {
-    //lets shows user names of all the pets
-    alert(nameArray)
+function addAge() {
+  //lets ask the user the age of her recorded pet
+let animalAge = prompt("What is the age of " + animalName + "?")
 }
 
+function addSpecies() {
+//ask the user the species of her chosen pet
+let addSpecies = prompt ("What species is " + animalName + "?")
 
+}
