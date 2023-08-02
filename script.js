dd4e48cd9dbd4b0e0482b6d13f9836510e52882c
Riley <124632055+pawseyri42@users.noreply.github.com>
2023-08-02 03:12:10 +0000

    remove done

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 
-let nameArray = [
+let pets = [
   { name: 'Tiger', age: 1, species: 'dog' },
   { name: 'Mouse', age: 5, species: 'horse' },
   { name: 'bubba', age: 3, species: 'lamb' },
@@ -7,11 +7,19 @@ let nameArray = [
 
 ]
 
+function showPets() {
+  for (let pet of pets) {
+    alert(pet.name)
+  }
+
+}
+
+
 function addPet() {
   // lets user input the name of the pet
   let name = prompt("What is the name of the pet?")
   let age = prompt("What is the age of " + name + "?")
-  let species = prompt ("What species is " + name + "?")
+  let species = prompt("What species is " + name + "?")
 
   // make an object called 'pet' from the details you got
   let pet = {
@@ -21,9 +29,21 @@ function addPet() {
   }
 
   // push the pet object onto the pets array
-  nameArray.push(pet)
+  pets.push(pet)
 }
 
-function animalRecord() {
-alert("button is working")
+function removePet() {
+  //output the names of each pet 
+  let index = 0
+  for (let pet of pets) {
+    alert(index + ":" + pet.name)
+    index = index + 1
+  }
+
+  //get the index of the pet to remove
+let indextoRemove = prompt ('which pet do you want to remove from the record?')
+
+  //Remove the pet at the index 
+pets.splice(indextoRemove, 1)
 }
+
